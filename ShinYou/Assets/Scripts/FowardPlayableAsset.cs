8facb31b6e024e4eb4ace437aed81e1c720fa85c
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-01-20 21:58:17 +0900

    手裏剣追加

---
+++ ShinYou/Assets/Scripts/FowardPlayableAsset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class FowardPlayableAsset : PlayableAsset
+{
+    public ExposedReference<GameObject> gameObject;
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        FowardBehaviour behaviour = new FowardBehaviour();
+        behaviour.targetObj = gameObject.Resolve(graph.GetResolver());
+        ScriptPlayable<FowardBehaviour> playable = ScriptPlayable<FowardBehaviour>.Create(graph, behaviour);
+        return playable;
+    }
+}
---
+++ ShinYou/Assets/Scripts/RotatePlayableAsset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class RotatePlayableAsset : PlayableAsset
+{
+    public ExposedReference<GameObject> gameObject;
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        RotateBehaviour behaviour = new RotateBehaviour();
+        behaviour.targetObj = gameObject.Resolve(graph.GetResolver());
+        ScriptPlayable<RotateBehaviour> playable = ScriptPlayable<RotateBehaviour>.Create(graph, behaviour);
+        return playable;
+    }
+}
